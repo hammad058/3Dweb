ec73bc7b68533abe893040b3d00889c4e3cecce8
hammad058 <56123908+hammad058@users.noreply.github.com>
2023-10-12 11:11:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Shery image effect command 
+// Shery.imageEffect(element, configurations)
+
+// Shery.imageMasker(".back" /* Element to target.*/, {
+//     //Parameters are optional.
+//     mouseFollower: true,
+//     text: "Ali",
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
+
+//   Shery.makeMagnet(".back" /* Element to target.*/, {
+//     //Parameters are optional.
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
+
+// Shery.textAnimate(".back" /* Element to target.*/, {
+//     //Parameters are optional.
+//     style: 1,
+//     y: 10,
+//     delay: 0.1,
+//     duration: 2,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     multiplier: 0.1,
+//   });
+
+// Shery.hoverWithMediaCircle(".back" /* Element to target.*/, {
+//     images: ["img_7564.jpg"] /*OR*/,
+//     //videos: ["video1.mp4", "video2.mp4"],
+//   });/Select Style
+//     debug: true, // Debug Panel
+//     config: {
+//       /* Config made from debug panel */
+//     },
+//     preset: "./presets/wigglewobble.json",
+//   });
+
+// Shery.imageEffect(".back", {
+//     style: 1 /*OR 5 for different variant */,
+//     debug: true,
+//   });
+
+
+// Shery.imageEffect(".back", {
+//     style: 2, /
+Shery.imageEffect(".back", {style:6, })
+
+Shery.imageMasker(".back" /* Element to target.*/, {
+    //Parameters are optional.
+    mouseFollower: true,
+    text: "Hammad",
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+  
